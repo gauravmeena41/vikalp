bb48e309582b1d1460011275a4b8f1e916da025c
gurumeena41 <gurumeena41.gm@gmail.com>
2022-07-08 18:09:29 +0530

    changes

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -17,23 +17,23 @@ export default async function handler(req: any, res: any) {
       role = 0,
     } = req.body;
     if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(email))
-      return res.json({ status: 0, message: "Invalid email" });
+      return res.status(200).json({ status: 0, message: "Invalid email" });
     else if (
       !/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm.test(password)
     )
-      return res.json({
+      return res.status(200).json({
         status: 0,
         message: "Password doesn't meet the security criteria",
       });
     else if (!/^(\+91[\-\s]?)?[0]?(91)?[789]\d{9}$/.test(phone))
-      return res.json({ status: 0, message: "Invalid phone number" });
+      return res.status(200).json({ status: 0, message: "Invalid phone number" });
     else if (!termsAndConditions)
-      return res.json({
+      return res.status(200).json({
         status: 0,
         message: "Please accept terms & conditions",
       });
     let user = await User.findOne({ email });
-    if (user) return res.json({ status: 0, message: "User already exists" });
+    if (user) return res.status(200).json({ status: 0, message: "User already exists" });
     user = await User.create({
       name,
       email,
@@ -43,7 +43,7 @@ export default async function handler(req: any, res: any) {
       termsAndConditions,
     });
 
-    let authToken:string = jwt.sign(
+    let authToken: string = jwt.sign(
       {
         id: user._id,
         role: user.role,
@@ -55,11 +55,14 @@ export default async function handler(req: any, res: any) {
       process.env.JWT_SECRET
     );
 
-    res.status(201).json({
+    res.status(200).json({
       status: 1,
       authToken,
     });
   } catch (error) {
-    console.log(error);
+    res.status(500).json({
+      status: 0,
+      message: error.message,
+    });
   }
-};
+}
