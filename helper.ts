29c2ec211f491f2332dd1d6bff9dfb280302608c
gaurav <gurumeena41.gm@gmail.com>
2022-10-10 17:17:03 +0530

    implemented aws ses for mail

--- helper.ts
+++ helper.ts
@@ -1,5 +1,14 @@
 // @ts-nocheck
 import nodemailer from "nodemailer";
+import AWS from "aws-sdk";
+
+const config = {
+  accessKeyId: process.env.IAM_USER_KEY,
+  secretAccessKey: process.env.IAM_USER_SECRET,
+  region: "ap-south-1",
+};
+
+let ses = new AWS.SES(config);
 
 let letters = [
   "A",
@@ -57,4 +66,68 @@ export const sendMail = async (mailOptions) => {
   } catch (error) {
     console.log(error);
   }
+};
+
+export const sendMailSES = ({ toAddresses, emailBody, emailSubject }) => {
+  for (let i = 0; i < toAddresses.length; i++) {}
+
+  emailBody += `<p><br><br><br>
+  <img width="30%" src="https://www.sama.live/assets/img/logo.png" />
+  <p style='color:red;font-size:10px'>
+  Note : The information contained in this e-mail and any attachments to this message are intended for the exclusive use of the intended recipient and may contain proprietary, confidential or legally privileged information. If you are not the intended recipient, please note that you are not authorised to disseminate, distribute or copy this e-mail or any parts of it or act upon/rely on the contents of this e-mail in any manner. Please notify the sender immediately by e-mail and destroy all copies of this e-mail and any attachments. Please also note that ODRways Solutions Private Limited or its subsidiaries and associated companies, (collectively 'ODRways'), are unable to exercise control or ensure or guarantee the integrity of/over the contents of the information contained in e-mail transmissions and that any views expressed in this e-mail are not endorsed by/binding on the ODRways unless the sender does so expressly with due authority of ODRways. Before opening any attachments please check them for viruses and defects and please note that ODRways accepts no liability or responsibility for any damage caused by any virus that may be transmitted by this email. Thank you for your cooperation.</p>`;
+
+  return new Promise((resolve, reject) => {
+    if (process.env.COMMUNICATION && process.env.COMMUNICATION == "0") {
+      console.log("no communication");
+      resolve("No communication");
+    } else {
+      console.log("comunication on");
+      ses.sendEmail(
+        {
+          Source: "Sama <noreply@mailers.sama.live>",
+          Destination: {
+            ToAddresses: toAddresses,
+          },
+          ReplyToAddresses: ["info@sama.live"],
+          Message: {
+            Subject: {
+              Charset: "UTF-8",
+              Data: emailSubject,
+            },
+            Body: {
+              Html: {
+                Charset: "UTF-8",
+                Data: emailBody,
+              },
+            },
+          },
+        },
+        (err, data) => {
+          //
+          if (err) {
+            resolve(err);
+          } else {
+            // setTimeout(async () => {
+            //   ses.getSendStatistics(keys)
+            // }, 5000);
+
+            console.log("Email Response -> ", {
+              responseId: data.MessageId,
+              requestId: data.ResponseMetadata.RequestId,
+              timestamp: new Date(Date.now()).toISOString(),
+              sender: "aws sns",
+              status: "pending",
+            });
+            resolve({
+              responseId: data.MessageId,
+              requestId: data.ResponseMetadata.RequestId,
+              timestamp: new Date(Date.now()).toISOString(),
+              sender: "aws sns",
+              status: "pending",
+            });
+          }
+        }
+      );
+    }
+  });
 };
--- pages/api/complaint/create.ts
+++ pages/api/complaint/create.ts
@@ -2,7 +2,7 @@
 
 import connectDb from "../../../utils/connectDb";
 import Complaint from "../../../models/Complaint";
-import { generateUniqueId, sendMail } from "../../../helper";
+import { generateUniqueId, sendMail, sendMailSES } from "../../../helper";
 
 connectDb();
 export default async function handler(req: any, res: any) {
@@ -38,34 +38,33 @@ export default async function handler(req: any, res: any) {
     });
 
     try {
-      await sendMail({
-        from: process.env.MAIL_EMAIL_ID,
-        to: complainantEmail,
-        subject: "Complaint Filed Successfully",
-        text: `Hi ${complainantName},\n
+      sendMailSES({
+        toAddresses: [respondentEmail],
+        emailSubject: "Complaint Filed against You",
+        emailBody: `Hello,\n
 
-        Your complaint was filed successfully. We have reached out to the other party and will have one of Sama’s expert Conciliators speak with you shortly.\n
-        
-        Sama’s Conciliators have helped resolve over 200,000 cases in the last year! Your case is in safe hands.\n
-        We look forward to helping you resolve this case peacefully.\n
-        
-        Suljhao, Magar Pyaar Se,\n
-        Team Sama`,
+        //   We are Team Sama! Sama helps people solve their legal problems without going to court, completely online. A complaint has been filed against you by <Complainant Name>. Don’t worry, Sama is here to listen to your side of the case too.\n
+  
+        //   Sama will help you and ${complainantName} resolve this issue peacefully with the help of an expert Sama Conciliator. Sama’s Conciliators are trained in resolving conflict and have already resolved over 200,000 cases for Sama! Your case is in safe hands.\n
+  
+        //   The Sama Conciliator will get in touch with you shortly.\n
+  
+        //   Suljhao, Magar Pyaar Se,\n
+        //   Team Sama`,
       });
-      await sendMail({
-        from: process.env.MAIL_EMAIL_ID,
-        to: respondentEmail,
-        subject: "Complaint Filed against You",
-        text: `Hello,\n
 
-        We are Team Sama! Sama helps people solve their legal problems without going to court, completely online. A complaint has been filed against you by <Complainant Name>. Don’t worry, Sama is here to listen to your side of the case too.\n
-        
-        Sama will help you and ${complainantName} resolve this issue peacefully with the help of an expert Sama Conciliator. Sama’s Conciliators are trained in resolving conflict and have already resolved over 200,000 cases for Sama! Your case is in safe hands.\n
-        
-        The Sama Conciliator will get in touch with you shortly.\n
-        
-        Suljhao, Magar Pyaar Se,\n
-        Team Sama`,
+      sendMailSES({
+        toAddresses: [complainantEmail],
+        emailSubject: "Complaint Filed against You",
+        emailBody: `Hi ${complainantName},\n
+
+        //   Your complaint was filed successfully. We have reached out to the other party and will have one of Sama’s expert Conciliators speak with you shortly.\n
+  
+        //   Sama’s Conciliators have helped resolve over 200,000 cases in the last year! Your case is in safe hands.\n
+        //   We look forward to helping you resolve this case peacefully.\n
+  
+        //   Suljhao, Magar Pyaar Se,\n
+        //   Team Sama`,
       });
     } catch (error) {
       console.log(error);
